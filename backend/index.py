1eb8914cb9513004d26103bf8def53fe7200bbce
Callum <59397232+cxllm@users.noreply.github.com>
2024-12-10 11:23:59 +0000

    Added comments and vercel configuration

--- backend/index.py
+++ backend/index.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# to ensure that the vercel deployment works
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
+
 from flask import Flask, request, jsonify, render_template
 from util.generate_password import (
     password_generator,
@@ -8,6 +14,8 @@ from util.generate_password import (
 default_length, default_uppercase, default_numbers, default_specialchars = (
     password_generator.__defaults__
 )
+
+
 # initialise the flask application
 app = Flask(
     __name__,
@@ -18,17 +26,20 @@ app = Flask(
 
 
 @app.get("/")
+@app.errorhandler(404)
 def root():
+    # the root function sends all requests at the root or an unknown page to react, which will handle it from there
     return render_template("index.html")
 
 
 @app.get("/api/generate-password")
 def generate_password():
     try:
+        # get the length and if it is invalid use the default length
         length = int(request.args.get("length", default=default_length))
     except:
         length = default_length
-
+    # get uppercase parameter and if it is invalid use the default case
     uppercase = request.args.get("uppercase", default=default_uppercase)
     match uppercase:
         case "False":
@@ -38,6 +49,7 @@ def generate_password():
         case _:
             uppercase = default_specialchars
 
+    # get numbers parameter and if it is invalid use the default case
     numbers = request.args.get("numbers", default=default_numbers)
     match numbers:
         case "False":
@@ -47,6 +59,7 @@ def generate_password():
         case _:
             numbers = default_specialchars
 
+    # get special characters parameter and if it is invalid use the default case
     specialchars = request.args.get("specialchars", default=default_specialchars)
     match specialchars:
         case "False":
@@ -56,12 +69,15 @@ def generate_password():
         case _:
             specialchars = default_specialchars
     try:
+        # generate the password and return it in a json format
         return jsonify(
             {"password": password_generator(length, uppercase, numbers, specialchars)}
         )
     except LengthTooLowError:
+        # return an error if it is too short
         return jsonify({"error": "The length of the password was not long enough"})
     except LengthTooHighError:
+        # return an error if it is too long
         return jsonify({"error": "The length of the password was too long"})
 
 
