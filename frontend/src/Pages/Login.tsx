a2d440f9014af05762243a90c882156db596c8d8
Callum <59397232+cxllm@users.noreply.github.com>
2025-01-14 17:54:09 +0000

    Added database integration to login and sign up pages, haven't fully implemented the functionality of them both yet

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,7 +1,43 @@
 import { Link } from "react-router";
+import { useState } from "react";
 
 // Login page
 function Login(props: { dark: boolean }) {
+	// Regex to verify if an email is valid
+	const emailRegex =
+		/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	// states for storing email and password
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const login = () => {
+		fetch("/api/login", {
+			method: "POST",
+			body: JSON.stringify({
+				email: email,
+				password: password
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then((r) => r.json())
+			.then((r) => {
+				switch (r.error) {
+					case "EMAIL_NOT_REGISTERED":
+					case "PASSWORD_NOT_CORRECT":
+						setError("The email and password combination is incorrect!");
+						break;
+					case undefined:
+						setError("");
+						break;
+					default:
+						setError("Internal Server Error");
+						break;
+				}
+				console.log(r);
+			});
+	};
 	return (
 		<>
 			<img
@@ -18,13 +54,19 @@ function Login(props: { dark: boolean }) {
 				Don't have an account? <Link to="/sign-up">Create an account</Link>
 			</p>
 			<form className="login">
-				<label htmlFor="email">E-Mail: </label>
+				<label htmlFor="email">Email: </label>
 				<input
 					type="text"
-					placeholder="Enter your e-mail address"
+					placeholder="Enter your email address"
 					required
 					name="email"
+					onInput={(v) => setEmail(v.currentTarget.value)}
 				/>
+				{email != "" && !emailRegex.test(email) ? (
+					<span className="red">Invalid email address entered!</span>
+				) : (
+					""
+				)}
 
 				<label>Password: </label>
 				<input
@@ -32,9 +74,19 @@ function Login(props: { dark: boolean }) {
 					placeholder="Enter your password"
 					required
 					name="password"
+					onInput={(v) => setPassword(v.currentTarget.value)}
 				/>
 			</form>
-			<button>Login</button>
+			{error ? <span className="red">{error}</span> : ""}
+			<button
+				onClick={login}
+				disabled={
+					// only allow the user to login if email is valid and password exists
+					!emailRegex.test(email) || !password
+				}
+			>
+				Login
+			</button>
 		</>
 	);
 }
