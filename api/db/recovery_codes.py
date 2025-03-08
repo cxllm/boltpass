82b03b1243c12a222c929cae93e08955a5ecbf95
Callum <59397232+cxllm@users.noreply.github.com>
2025-03-08 16:20:50 +0000

    Comments on recovery codes

--- api/db/recovery_codes.py
+++ api/db/recovery_codes.py
@@ -8,9 +8,18 @@ from util.password_hashing import verify_password
 
 
 class RecoveryCode:
-    def __init__(self, user_id, code_id):
+    def __init__(self, user_id: str, code_id: str) -> None:
+        """
+        Initialises the recovery code class
+
+            Parameters:
+                self: The specific instance of the class
+                user_id (str): The user ID associated with the code
+                code_id (str): The code ID associated with the code
+        """
         self.user_id = user_id
         self.code_id = code_id
+        # Get information from database. Verification not needed as it will only be called in a context where it is known to exist.
         conn, cursor = connect()
         cursor.execute(
             "SELECT * FROM recovery_codes WHERE user_id=%s AND code_id=%s",
@@ -20,9 +29,25 @@ class RecoveryCode:
         conn.close()
 
     def verify(self, code):
+        """
+        Verify if a recovery code entered is the right code
+
+            Parameters:
+                self: The specific instance of the class
+                code (str): The code to verify
+
+            Returns:
+                out (bool): Whether the code is correct or not
+        """
         return verify_password(code, self.salt, self.hashed)
 
     def delete(self):
+        """
+        Deletes a recovery code
+
+            Parameters:
+                self: The specific instance of the class
+        """
         conn, cursor = connect()
         cursor.execute(
             "DELETE FROM recovery_codes WHERE user_id = %s AND code_id = %s",
